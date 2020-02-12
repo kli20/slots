892791a8a870366336d0a3f94d6e37870202855f
Kate Li <kli20@punahou.edu>
2020-02-12 18:11:15 +0000

    Added header

---
+++ Slots.java
@@ -0,0 +1,11 @@
+// by Kate Li
+
+import java.util.Scanner;
+
+public class Slots
+{
+    public static void main (String[] args)
+    {
+        System.out.print("Spin to win a coupon! ");
+    }
+}
