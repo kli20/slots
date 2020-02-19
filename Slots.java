c7ba6a253b66637c6a6cf340b5eee2f6deecb811
Kate Li <kli20@punahou.edu>
2020-02-19 17:55:35 +0000

    Switch numbers for symbols

--- Slots.java
+++ Slots.java
@@ -1,11 +1,87 @@
 // by Kate Li
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Slots
 {
     public static void main (String[] args)
     {
-        System.out.print("Spin to win a coupon! ");
+        System.out.println("Spin to win a coupon! ");
+        int slot1 = (int) Math.round(Math.random() * 10);
+        int slot2 = (int) Math.round(Math.random() * 10);
+        int slot3 = (int) Math.round(Math.random() * 10);
+
+        System.out.print("You spinned ");
+        // Match slot number to a symbol
+        switch(slot1)
+        {
+            case 1:
+            case 2:
+                System.out.print("☆ ");
+                break;
+            case 3:
+            case 4:
+                System.out.print("♡ ");
+                break;
+            case 5:
+            case 6:
+                System.out.print("✧ ");
+                break;
+            case 7:
+            case 8:
+                System.out.print("❀ ");
+                break;
+            case 9:
+            case 10:
+                System.out.print("☽ ");
+                break;
+        }
+        switch(slot2)
+        {
+            case 1:
+            case 2:
+                System.out.print("☆ ");
+                break;
+            case 3:
+            case 4:
+                System.out.print("♡ ");
+                break;
+            case 5:
+            case 6:
+                System.out.print("✧ ");
+                break;
+            case 7:
+            case 8:
+                System.out.print("❀ ");
+                break;
+            case 9:
+            case 10:
+                System.out.print("☽ ");
+                break;
+        }
+        switch(slot3)
+        {
+            case 1:
+            case 2:
+                System.out.println("☆ ");
+                break;
+            case 3:
+            case 4:
+                System.out.println("♡ ");
+                break;
+            case 5:
+            case 6:
+                System.out.println("✧ ");
+                break;
+            case 7:
+            case 8:
+                System.out.println("❀ ");
+                break;
+            case 9:
+            case 10:
+                System.out.println("☽ ");
+                break;
+        }
+
     }
 }
