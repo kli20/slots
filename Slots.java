6e8b39f8ab19956be01e033b49910c5199210a32
Kate Li <kli20@punahou.edu>
2020-02-28 06:33:41 +0000

    Cleaned up code and added ReadMe

--- Slots.java
+++ Slots.java
@@ -12,10 +12,7 @@ public class Slots
         int slot2 = (int) Math.round(Math.random() * 10);
         int slot3 = (int) Math.round(Math.random() * 10);
 
-        System.out.println("You spinned " + slot1 + " " + slot2 + " " + slot3);
-
-        // System.out.print("You spinned ");
-
+        // Creates array for 3 slots
         int[] symslot = new int[3];
         symslot[0] = 0;
         symslot[1] = 0;
@@ -25,125 +22,35 @@ public class Slots
         System.out.print(symbol(slot2, symslot, 1));
         System.out.println(symbol(slot3, symslot, 2));
 
-        // Match slot number to a symbol
-        // switch(slot1)
-        // {
-        //     case 0:
-        //     case 1:
-        //     case 2:
-        //         System.out.print("☆ ");
-        //         symslot1 = 1;
-        //         break;
-        //     case 3:
-        //     case 4:
-        //         System.out.print("♡ ");
-        //         symslot1 = 2;
-        //         break;
-        //     case 5:
-        //     case 6:
-        //         System.out.print("✧ ");
-        //         symslot1 = 3;
-        //         break;
-        //     case 7:
-        //     case 8:
-        //         System.out.print("❀ ");
-        //         symslot1 = 4;
-        //         break;
-        //     case 9:
-        //     case 10:
-        //         System.out.print("☽ ");
-        //         symslot1 = 5;
-        //         break;
-        // }
-        // switch(slot2)
-        // {
-        //     case 0:
-        //     case 1:
-        //     case 2:
-        //         System.out.print("☆ ");
-        //         symslot2 = 1;
-        //         break;
-        //     case 3:
-        //     case 4:
-        //         System.out.print("♡ ");
-        //         symslot2 = 2;
-        //         break;
-        //     case 5:
-        //     case 6:
-        //         System.out.print("✧ ");
-        //         symslot2 = 3;
-        //         break;
-        //     case 7:
-        //     case 8:
-        //         System.out.print("❀ ");
-        //         symslot2 = 4;
-        //         break;
-        //     case 9:
-        //     case 10:
-        //         System.out.print("☽ ");
-        //         symslot2 = 5;
-        //         break;
-        // }
-        // switch(slot3)
-        // {
-        //     case 0:
-        //     case 1:
-        //     case 2:
-        //         System.out.println("☆ ");
-        //         symslot3 = 1;
-        //         break;
-        //     case 3:
-        //     case 4:
-        //         System.out.println("♡ ");
-        //         symslot3 = 2;
-        //         break;
-        //     case 5:
-        //     case 6:
-        //         System.out.println("✧ ");
-        //         symslot3 = 3;
-        //         break;
-        //     case 7:
-        //     case 8:
-        //         System.out.println("❀ ");
-        //         symslot3 = 4;
-        //         break;
-        //     case 9:
-        //     case 10:
-        //         System.out.println("☽ ");
-        //         symslot3 = 5;
-        //         break;
-        // }
-
-        // System.out.println("Your symslot is " + symslot1 + " " + symslot2 + " " + symslot3);
-
         // If 2 match
         if (symslot[0] == symslot[1] || symslot[0] == symslot[2] || symslot[1] == symslot[2] )
         {
-            System.out.println("Two of your symbols matched! Use code __ for 20% off");
+            System.out.println("Two of your symbols matched! Use code TWENTY for 20% off at https://preciousbykate.com. ");
         }
-        // If 3 match (Jackpot)
+        // If 3 match (jackpot)
         else if (symslot[0] == symslot[1] && symslot[1] == symslot[2])
         {
-            System.out.println("Jackpot winner!! 50% off");
+            System.out.println("Congrats, you're a jackpot winner!! Use code FIFTY for 50% off at https://preciousbykate.com.");
         }
-        // No matches
+        // If no matches
         else
         {
-            System.out.println("No matches :( 10% off");
+            System.out.println("No matches, but no worries! Use code TEN for 10% off at https://preciousbykate.com. ");
         }
-
     }
 
     private static String symbol (int slotnum, int[] symslot, int wheel)
     {
         String output = "";
-        // update symslot wheel with symbols, assigns number to corresponding symbol
+        // Updates symslot wheel with symbols, assigns number to corresponding symbol
         switch(slotnum)
         {
             case 0:
             case 1:
             case 2:
+                // Matches number to corresponding symbol
                 output = "☆ ";
+                // Assigns number to that symbol
                 symslot[wheel] = 1;
                 break;
             case 3:
